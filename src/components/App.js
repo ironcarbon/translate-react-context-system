edb1ccf78e8004ef270f3924524bd90741acd44b
ipek <ipekcivicakan@gmail.com>
2019-01-05 18:04:43 -0800

    selecting a language, and creating form

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import UserCreate from './UserCreate';
 
 class App extends React.Component {
+    state = { language: 'english' };
+
+    onLanguageChange = language => {
+        return this.setState({ language: language })
+    }
+
     render() {
         return (
-            <div className="ui container">App</div>
+            <div className="ui container">
+                <div>
+                    Select a language:
+                    <i className="us flag" onClick={() => this.onLanguageChange('english')}></i>
+                    <i className="tr flag" onClick={() => this.onLanguageChange('turkish')}></i>
+                </div>
+                {this.state.language}
+                <UserCreate />
+            </div>
         )
     }
 }
